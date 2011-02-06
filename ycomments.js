d573553f5c1480fbcfb8633abdc2aeae1bb76bae
Will Maier <willmaier@ml1.net>
2011-02-05 20:14:14 -0600

    Grey-out unpopular comments.

--- ycomments.js
+++ ycomments.js
@@ -47,7 +47,10 @@
                 thread.postedAgo + " | " + $.fn.ycomments.postid(thread.id, "link") +
             "</p></header>";
 
-        html += "<p class='ycomments-comment-text'>" + $.fn.ycomments.comment(thread.comment) + "</p>";
+        html += "<p class='ycomments-comment-text";
+        if (thread.points < 1)
+            html += " ycomments-unpopular";
+        html += "'>" + $.fn.ycomments.comment(thread.comment) + "</p>";
         html += "<p>" + $.fn.ycomments.reply(thread.id, thread.parentId) + "</p>";
 
         $.each(thread.children, function () {
